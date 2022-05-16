27ff7ad1a4025a8ecffa306183819d4e57e2fbe9
youen froger <youen.froger2@gmail.com>
2022-05-17 01:13:22 +0200

    small server issues fixed

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%=h#^li28b&(4i=6r99dl=g@ipt=ni34zkfy=1ld7cl7ffvi3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.laen.fr", "127.0.0.1"]
 
 
 # Application definition
