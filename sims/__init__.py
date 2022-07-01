62700941f6575bb4428586aec2a95b4287529c6c
youen froger <youen.froger2@gmail.com>
2022-07-01 12:24:13 +0200

    add of consumption scaling in simulation

--- sims/__init__.py
+++ sims/__init__.py
@@ -1,15 +1,17 @@
 import numpy as np
 from .modules.predim.dataLoader import *
 from .modules.predim.sim import SimParams
+from .modules.predim.configuration import config
 import os
 import time
 thisPath = os.path.dirname(__file__)
 print(thisPath)
 dl = dataloader()
-prod_eol = dl.load_prod(thisPath + "/data/Prod Eol Elfe.csv")
-user     = dl.load_one_user(thisPath +"/data/averageUser0.csv")
-prod_sol = dl.load_solar_panel_prod(thisPath + "/data/production_bretagne/Solaire.csv")
-prod_bio = dl.load_solar_panel_prod(thisPath + "/data/production_bretagne/Bioenergie.csv")
+prod_eol   = dl.load_prod(thisPath + "/data/Prod Eol Elfe.csv")
+user       = dl.load_one_user(thisPath +"/data/averageUser0.csv")
+prod_sol   = dl.load_solar_panel_prod(thisPath + "/data/production_bretagne/Solaire.csv")
+prod_bio   = dl.load_solar_panel_prod(thisPath + "/data/production_bretagne/Bioenergie.csv")
+per_user_conso = (config.CA_PONTCHATEAU_RES_CONSUMPTION + config.CA_REDON_RES_CONSUMPTION) / (config.CA_REDON_POPULATION + config.CA_PONTCHATEAU_POPULATION)
 basic_sim_params = SimParams(
 	has_solar                         = True,
 	has_bioenergy                     = True,
@@ -20,14 +22,14 @@ basic_sim_params = SimParams(
 	has_wind_scaling                  = True,
 	has_bioenergy_scaling             = True,
 	has_piloted_bioenergy_scaling     = True,
-	has_consumer_scaling              = False,
+	has_consumer_scaling              = True,
 	solar_power                       = 0.0,
 	wind_power                        = 0.0,
 	bioenergy_power                   = 0.0,
 	battery_capacity                  = 0.0,
 	piloted_bioenergy_power           = 0.0,
 	flexibility_ratio                 = 0.0,
-	consumer_power                    = 0.0,
+	consumer_power                    = per_user_conso,
 	consumer_contrib                  = None,
 	solar_curve                       = prod_sol,
 	wind_curve                        = prod_eol,
